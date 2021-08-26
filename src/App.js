bb5b775fdf9fe417d039a72cfb237177b3d04755
OmarArias <omarxarias@gmail.com>
2021-08-26 10:27:38 -0400

    Cuarto Paso 28 de Agosto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 const nombre=document.querySelector("#nombre-input");
+const edad=document.querySelector("#edad-input");
 const sexo=document.querySelector("#sexo-input");
 const form=document.querySelector("#saludador-form");
 
 form.addEventListener("submit", (event) => {
-    if(sexo.value == 'Hombre'){
-        alert("Hola señor " + nombre.value);
+    if(edad.value > 18){
+        if(sexo.value == 'Hombre'){
+            alert("Hola señor " + nombre.value);
+        }else{
+            alert("Hola señora " + nombre.value);
+        }
     }else{
-        alert("Hola señora " + nombre.value);
+        if(sexo.value == 'Hombre'){
+            alert("Hola joven " + nombre.value);
+        }else{
+            alert("Hola señorita " + nombre.value);
+        }
     }
 });
