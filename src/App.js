d0689fc0f89fa1931f6d49d4f659b0187b6c1fe2
OmarArias <omarxarias@gmail.com>
2021-08-26 10:26:31 -0400

    Tercer paso 28 de Agosto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 const nombre=document.querySelector("#nombre-input");
+const sexo=document.querySelector("#sexo-input");
 const form=document.querySelector("#saludador-form");
 
-
 form.addEventListener("submit", (event) => {
-    alert("Hola " + nombre.value);
+    if(sexo.value == 'Hombre'){
+        alert("Hola señor " + nombre.value);
+    }else{
+        alert("Hola señora " + nombre.value);
+    }
 });
